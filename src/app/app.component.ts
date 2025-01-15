d698a19a9d91b76a1d4800760b87cc75a612319e
Eddie <macedoe@gmail.com>
2025-01-14 16:32:53 -0800

    Add service worker support and manifest for PWA; update version to 1.0.1; enhance app component with install prompt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 import { FooterComponent, SidenavComponent, TopBarComponent } from './common/components';
 import { SidenavService } from './common/services';
 import { MaterialModule } from './material.module';
@@ -15,11 +16,49 @@ export class AppComponent implements OnInit {
     @ViewChild('sidenav') sidenav!: MatSidenav;
     title = 'simple bible search';
 
-    constructor(private sidenavService: SidenavService) {}
+    deferredPrompt: any;
+    showInstallButton = false;
+
+    constructor(
+        private sidenavService: SidenavService,
+        private swUpdate: SwUpdate
+    ) {}
 
     ngOnInit() {
         this.sidenavService.toggleSidenav.subscribe(() => {
             this.sidenav.toggle();
         });
+
+        if (this.swUpdate.isEnabled) {
+            this.swUpdate.versionUpdates.subscribe(event => {
+                if (event.type === 'VERSION_READY') {
+                    const userConfirmed = confirm('A new version of the app is available. Do you want to load it?');
+                    if (userConfirmed) {
+                        window.location.reload();
+                    }
+                }
+            });
+        }
+    }
+
+    @HostListener('window:beforeinstallprompt', ['$event'])
+    onBeforeInstallPrompt(event: Event) {
+        event.preventDefault();
+        this.deferredPrompt = event;
+        this.showInstallButton = true;
+    }
+
+    installPwa() {
+        if (this.deferredPrompt) {
+            this.deferredPrompt.prompt();
+            this.deferredPrompt.userConfirmed.then((choiceResult: any) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the install prompt');
+                } else {
+                    console.log('User dismissed the install prompt.');
+                }
+                this.deferredPrompt = null;
+            });
+        }
     }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { bibleApiInterceptor } from './common/interceptors';
 import { ConfigService } from './common/services';
 import { loadApiConfiguration } from './common/utils';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -16,6 +17,9 @@ export const appConfig: ApplicationConfig = {
         provideHttpClient(withInterceptors([bibleApiInterceptor])),
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes, withHashLocation()),
-        provideAnimationsAsync()
+        provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
     ]
 };

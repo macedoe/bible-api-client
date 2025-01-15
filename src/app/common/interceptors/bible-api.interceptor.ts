76b2887ea60f7871bdb143e6c2de944fe6504bf9
Eddie <macedoe@gmail.com>
2025-01-14 21:43:41 -0800

    Update service worker configuration and API interceptor; bump version to 1.0.1-3

--- src/app/common/interceptors/bible-api.interceptor.ts
+++ src/app/common/interceptors/bible-api.interceptor.ts
@@ -1,5 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const bibleApiInterceptor: HttpInterceptorFn = (req, next) => {
+    if (req.url.includes('bible-api.com')) {
+        const request = req.clone().headers.append('Content-Type', 'application/json;charset=utf-8');
+    }
     return next(req);
 };

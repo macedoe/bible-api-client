d866debc76d1c0b1b0c6d51e378bb7597c16ab74
Eddie <macedoe@gmail.com>
2025-01-16 20:37:45 -0800

    Bump version to 1.0.1-9; migrate cookie management to localStorage and update scripture service methods

--- src/app/features/scripture/services/scripture.service.ts
+++ src/app/features/scripture/services/scripture.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { CookieService } from 'ngx-cookie-service';
 import { BIBLE_BOOK_COOKIE, BIBLE_CHAPTER_COOKIE, BIBLE_TRANSLATION_COOKIE } from '../../../common/constants';
 import { BibleBook, BibleChapter, BibleTranslation, BibleVerse } from '../../../common/interfaces';
 import { BibleApiService } from '../../../common/services';
@@ -23,8 +22,7 @@ export class ScriptureService {
 
     constructor(
         formBuilder: FormBuilder,
-        private bibleApiService: BibleApiService,
-        private cookieService: CookieService
+        private bibleApiService: BibleApiService
     ) {
         this.selectForm = formBuilder.group({
             translation: ['', Validators.required]
@@ -40,7 +38,7 @@ export class ScriptureService {
 
         let webTranslation: BibleTranslation | null = null;
 
-        const translationCookie = this.cookieService.get(BIBLE_TRANSLATION_COOKIE);
+        const translationCookie = localStorage.getItem(BIBLE_TRANSLATION_COOKIE);
         if (translationCookie) {
             webTranslation = JSON.parse(translationCookie) as BibleTranslation;
         }
@@ -55,12 +53,12 @@ export class ScriptureService {
             this.selectedBooks = translation.books;
         }
 
-        const bookCookie = this.cookieService.get(BIBLE_BOOK_COOKIE);
+        const bookCookie = localStorage.getItem(BIBLE_BOOK_COOKIE);
         if (bookCookie) {
             await this.onBookSelected(JSON.parse(bookCookie) as BibleBook);
         }
 
-        const chapterCookie = this.cookieService.get(BIBLE_CHAPTER_COOKIE);
+        const chapterCookie = localStorage.getItem(BIBLE_CHAPTER_COOKIE);
         if (chapterCookie) {
             await this.onChapterSelected(JSON.parse(chapterCookie) as BibleChapter);
         }
@@ -70,7 +68,7 @@ export class ScriptureService {
         if (this.selectedTranslation) {
             const translation = await this.bibleApiService.getCachedTranslation(this.selectedTranslation);
 
-            this.cookieService.set(BIBLE_TRANSLATION_COOKIE, JSON.stringify(translation.translation));
+            localStorage.setItem(BIBLE_TRANSLATION_COOKIE, JSON.stringify(translation.translation));
 
             this.selectedBooks = translation.books;
         }
@@ -83,7 +81,7 @@ export class ScriptureService {
     public async onBookSelected(book: BibleBook) {
         this.selectedBook = book;
 
-        this.cookieService.set(BIBLE_BOOK_COOKIE, JSON.stringify(this.selectedBook));
+        localStorage.setItem(BIBLE_BOOK_COOKIE, JSON.stringify(this.selectedBook));
 
         if (this.selectedBook.id !== this.selectedChapter?.book_id) {
             this.selectedBookChapters = null;
@@ -99,7 +97,7 @@ export class ScriptureService {
     public async onChapterSelected(chapter: BibleChapter) {
         this.selectedChapter = chapter;
 
-        this.cookieService.set(BIBLE_CHAPTER_COOKIE, JSON.stringify(this.selectedChapter));
+        localStorage.setItem(BIBLE_CHAPTER_COOKIE, JSON.stringify(this.selectedChapter));
 
         const selectedTranslation = this.selectForm.get('translation')?.value as string | null;
         if (selectedTranslation) {
